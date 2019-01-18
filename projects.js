99a9c66a884535b0fc39795e05fdb36ff8c5a545
jheinz1999 <jonathan.heinz@aol.com>
2019-01-18 08:58:12 -0800

    POST to /api/projects works

--- projects.js
+++ projects.js
@@ -24,4 +24,40 @@ server.get('/', async (req, res) => {
 
 });
 
+server.post('/', async (req, res) => {
+
+  let { name, description, completed } = req.body;
+
+  if (!name) {
+
+    res.status(statusCodes.bad_req).json({message: 'No name provided'});
+    return;
+
+  }
+
+  if (completed === undefined) {
+
+    completed = false;
+
+  }
+
+  try {
+
+    const id = await db.addProject({ name, description, completed });
+
+    const data = await db.getProject(id[0]);
+
+    res.status(statusCodes.ok).json(data);
+
+  }
+
+  catch (err) {
+
+    console.log(err);
+    genericErr(err, res);
+
+  }
+
+});
+
 module.exports = server;
