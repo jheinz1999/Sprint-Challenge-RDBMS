f294d1c3de6e20b1d0250d71f7666a2278c078a6
jheinz1999 <jonathan.heinz@aol.com>
2019-01-18 09:11:56 -0800

    GET and POST to /api/actions work

---
+++ actions.js
@@ -0,0 +1,91 @@
+const express = require('express');
+
+const db = require('./data/db');
+const genericErr = require('./common/err');
+const statusCodes = require('./common/statusCodes');
+
+const server = express.Router();
+
+server.get('/', async (req, res) => {
+
+  try {
+
+    const data = await db.getActions();
+
+    res.status(statusCodes.ok).json(data);
+
+  }
+
+  catch (err) {
+
+    genericErr(err, res);
+
+  }
+
+});
+
+server.get('/:id', async (req, res) => {
+
+  const { id } = req.params;
+
+  try {
+
+    const data = await db.getAction(id);
+
+    res.status(statusCodes.ok).json(data[0]);
+
+  }
+
+  catch (err) {
+
+    console.log(err);
+    res.status(statusCodes.not_found).json({message: 'Action not found'});
+
+  }
+
+});
+
+server.post('/', async (req, res) => {
+
+  let { description, notes, completed, project_id } = req.body;
+
+  if (!description) {
+
+    res.status(statusCodes.bad_req).json({message: 'No description provided'});
+    return;
+
+  }
+
+  if (completed === undefined) {
+
+    completed = false;
+
+  }
+
+  if (!project_id) {
+
+    res.status(statusCodes.bad_req).json({message: 'No project ID provided'});
+    return;
+
+  }
+
+  try {
+
+    const id = await db.addAction({ description, notes, completed, project_id });
+
+    const data = await db.getAction(id[0]);
+
+    res.status(statusCodes.created).json(data);
+
+  }
+
+  catch (err) {
+
+    console.log(err);
+    genericErr(err, res);
+
+  }
+
+});
+
+module.exports = server;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const projectRouter = require('./projects');
+const actionRouter = require('./actions');
 
 const server = express();
 
 server.use(express.json());
 server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
 
 module.exports = server;
